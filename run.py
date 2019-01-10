b94cb714297a9c3135455b0e30d8a39b7b8ca5ad
Ethan Houston <ethan.houston@gmail.com>
2019-01-09 16:12:15 -0800

    Add screenshot module

--- run.py
+++ run.py
@@ -2,6 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 import json
 import sys
+import sqlite3
 
 
 def make_json(filename, usernames):
@@ -11,6 +12,11 @@ def make_json(filename, usernames):
         filename = filename + ".csv"
     f = open(filename, "w")
     f.write("username,name,account_type,public,description,followers,following,posts\n")
+    conn = sqlite3.connect('accounts.db')
+    c = conn.cursor()
+    c.execute('''CREATE TABLE IF NOT EXISTS accounts (username text, name text, account_type text, public numeric, 
+    description text, followers integer, following integer, posts integer, UNIQUE (username))''')
+    conn.commit()
     for user in usernames:
         try:
             html = requests.get("https://www.instagram.com/{0}/".format(user)).text
@@ -40,11 +46,11 @@ def make_json(filename, usernames):
             account_type = ""
         if public:
             try:
-                name = data["name"].replace(",", " ")
+                name = data["name"].replace(",", " ").replace("、", " ").replace("，", " ")
             except KeyError:
                 name = ""
         try:
-            bio_description = " ".join(data["description"].replace(",", " ").split())
+            bio_description = " ".join(data["description"].replace(",", " ").replace("、", " ").replace("，", " ").split())
         except KeyError:
             bio_description = ""
 
@@ -53,17 +59,31 @@ def make_json(filename, usernames):
         for tag in soup.find_all("meta"):
             if tag.get("property", None) == "og:description":
                 description = tag.get("content", None)
-                # TODO: Clean these up, shouldn't have to re-do these operations
-                followers = description.split("Followers")[0].replace(',', '').strip()
-                following = description.split("Followers")[1].split("Following")[0].replace(',', '').strip()
-                posts = description.split("Posts")[0].split("Following")[1].replace(',', '').strip()
+
+                first_block = description.split("Followers")
+
+                followers = parse_number(first_block[0].replace(',', '').strip())
+                following = parse_number(first_block[1].split("Following")[0].replace(',', '').strip())
+                posts = parse_number(description.split("Posts")[0].split("Following")[1].replace(',', '').strip())
 
                 f.write("{0},{1},{2},{3},{4},{5},{6},{7}\n".format(username, name, account_type, public,
                                                                bio_description, following, followers, posts))
                 count += 1
+                c.execute("INSERT INTO accounts VALUES (\'%s\', \'%s\', \'%s\', %d, \'%s\', %d, %d, %d)" %
+                          (username, name, account_type, public, bio_description, followers, following, posts))
+                conn.commit()
                 print("Completed {0} out of {1}".format(count, len(usernames)))
 
     return data
+
+
+def parse_number(num):
+    if num[-1] == "k":
+        return int(float(num[:-1]) * 1000)
+    elif num[-1] == "m":
+        return int(float(num[:-1]) * 1_000_000)
+    else:
+        return int(num)
 
 
 if len(sys.argv) != 1:
---
+++ screenshot.py
@@ -0,0 +1,25 @@
+import asyncio
+from pyppeteer import launch
+import sqlite3
+
+conn = sqlite3.connect('accounts.db')
+c = conn.cursor()
+c.execute('SELECT username FROM accounts')
+column = c.fetchall()
+
+
+async def main():
+    total = len(column)
+    count = 0
+    for user in column:
+        browser = await launch()
+        page = await browser.newPage()
+        await page.setViewport({'width': 960, 'height': 1200})
+        await page.goto('https://instagram.com/%s' % user)
+        await page.screenshot({'path': 'screenshots/%s.jpeg' % user})
+        await browser.close()
+        count += 1
+        print("{0} out of {1} completed".format(count, total))
+
+
+asyncio.get_event_loop().run_until_complete(main())
